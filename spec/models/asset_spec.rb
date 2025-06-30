75e5e4fc2c9ce33d4e9e6a7d2e4a8117c883b7b8
haidang-lhd <haidang.lhd.lhd@gmail.com>
2025-06-30 22:05:23 +0700

    Add tests for manual price setting behavior and update Sidekiq configuration

--- spec/models/asset_spec.rb
+++ spec/models/asset_spec.rb
@@ -16,5 +16,18 @@ RSpec.describe Asset, type: :model do
       expect(price_record.price).to eq(123.45)
       expect(price_record.synced_at).to be_within(1.second).of(Time.current)
     end
+
+    context 'method behavior' do
+      it 'returns the latest price after setting a new price' do
+        asset.manual_set_price(123.45)
+        expect(asset.latest_price.price).to eq(123.45)
+      end
+
+      it 'updates the synced_at timestamp to current time' do
+        initial_time = Time.current
+        asset.manual_set_price(123.45)
+        expect(asset.latest_price.synced_at).to be_within(1.second).of(initial_time)
+      end
+    end
   end
 end
