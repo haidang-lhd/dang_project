566bfe1b8b3db16fda89b3e6093568791ec5c311
haidang-lhd <haidang.lhd.lhd@gmail.com>
2025-08-10 20:49:18 +0700

    Add cryptocurrency asset syncing and seed data for investment transactions

--- app/models/cryptocurrency_asset.rb
+++ app/models/cryptocurrency_asset.rb
@@ -21,4 +21,59 @@
 #  fk_rails_...  (category_id => categories.id)
 #
 class CryptocurrencyAsset < Asset
+    COINGECKO_IDS = {
+    'BTC'  => 'bitcoin',
+    'ETH'  => 'ethereum',
+    'BNB'  => 'binancecoin',
+    'USDT' => 'tether',
+    'USDC' => 'usd-coin',
+    'SOL'  => 'solana',
+    'ADA'  => 'cardano',
+    'XRP'  => 'ripple',
+    'DOGE' => 'dogecoin',
+    'TRX'  => 'tron',
+    'DOT'  => 'polkadot',
+    'MATIC'=> 'polygon',
+    'AVAX' => 'avalanche-2',
+    'TON'  => 'the-open-network'
+  }.freeze
+
+  def sync_price
+    symbol = name.to_s.upcase.strip
+    cg_id  = COINGECKO_IDS[symbol] || symbol.downcase
+
+    url = URI("https://api.coingecko.com/api/v3/simple/price?ids=#{CGI.escape(cg_id)}&vs_currencies=vnd")
+
+    begin
+      req = Net::HTTP::Get.new(url)
+      api_key = ENV['COINGECKO_API_KEY']
+      req['x-cg-demo-api-key'] = api_key if api_key.present?
+
+      res = Net::HTTP.start(url.host, url.port, use_ssl: true, read_timeout: 10) do |http|
+        http.request(req)
+      end
+
+      if res.is_a?(Net::HTTPSuccess)
+        data = JSON.parse(res.body) rescue {}
+        vnd  = data.dig(cg_id, 'vnd')
+
+        if vnd
+          asset_prices.create!(price: vnd.to_f, synced_at: Time.current)
+          return vnd.to_f
+        else
+          Rails.logger.warn "CoinGecko returned no VND price for #{name} (id: #{cg_id}). Body: #{res.body}"
+          asset_prices.create!(price: 0.0, synced_at: Time.current)
+          return 0.0
+        end
+      else
+        Rails.logger.error "HTTP #{res.code} from CoinGecko for #{name} (id: #{cg_id}). Body: #{res.body}"
+        asset_prices.create!(price: 0.0, synced_at: Time.current)
+        return 0.0
+      end
+    rescue StandardError => e
+      Rails.logger.error "Failed to sync crypto price for #{name}: #{e.class} - #{e.message}\n#{e.backtrace.first(10).join("\n")}"
+      asset_prices.create!(price: 0.0, synced_at: Time.current)
+      return 0.0
+    end
+  end
 end
