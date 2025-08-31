c6ca7290a5aad19ac76acdbb3456b07fb9db48c8
haidang-lhd <haidang.lhd.lhd@gmail.com>
2025-08-31 17:09:12 +0700

    fix fail spec

--- spec/support/auth_helper.rb
+++ spec/support/auth_helper.rb
@@ -11,7 +11,7 @@ module AuthHelper
   def jwt_token_for(user)
     JWT.encode(
       { user_id: user.id, exp: 24.hours.from_now.to_i },
-      Rails.application.secret_key_base
+      Rails.application.credentials.jwt_secret_key || Rails.application.secret_key_base
     )
   end
 end
